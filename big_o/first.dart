8c0d0de6bf6fe60ae6d8f03ed5c5d710bbd59a35
diyorkhud <d1yorkhud@gmail.com>
2024-04-05 12:18:17 +0500

    added performance measuring function

--- big_o/first.dart
+++ big_o/first.dart
@@ -1,13 +1,20 @@
+import 'dart:core';
+
 void main() {
   var nemo = ['nemo'];
-  print(findNemo(nemo));
+  var large = List.filled(10000, 'nemo');
+  print(findNemo(large));
 }
 
 findNemo(List array) {
+  final stopwatch = Stopwatch();
+  stopwatch.start();
   for (int i = 0; i < array.length; i++) {
     if (array[i] == 'nemo') {
       print('Found Nemo');
     }
   }
+  stopwatch.stop();
+  print('Elapsed time: ${stopwatch.elapsedMilliseconds} milliseconds');
   return 'Mission Completed!';
 }
