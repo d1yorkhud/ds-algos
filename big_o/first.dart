1908771f991a00b4ae5ea493e06eb6a60c0aa9f8
diyorkhud <d1yorkhud@gmail.com>
2024-04-05 11:54:15 +0500

    finding nemo exercise

--- big_o/first.dart
+++ big_o/first.dart
@@ -1,3 +1,13 @@
 void main() {
-  print('kmnmala');
+  var nemo = ['nemo'];
+  print(findNemo(nemo));
+}
+
+findNemo(List array) {
+  for (int i = 0; i < array.length; i++) {
+    if (array[i] == 'nemo') {
+      print('Found Nemo');
+    }
+  }
+  return 'Mission Completed!';
 }
