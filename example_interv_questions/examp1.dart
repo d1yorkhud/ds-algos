d655598b2e5f1cb0c732a156da869a473c6e639d
diyorkhud <d1yorkhud@gmail.com>
2024-04-09 21:56:35 +0500

    added 3rd solution via dart built-in method

--- example_interv_questions/examp1.dart
+++ example_interv_questions/examp1.dart
@@ -15,10 +15,12 @@ void main() {
 
   print(containsCommonItem(array1, array2));
   print(containsCommonItem2(array1, array2));
+  print(containsCommonItem3(array1, array2));
 }
 
 // brute force approach.
 // Time Complexity O(a*b)
+// Space Complexity O(1)
 bool containsCommonItem(List arr1, List arr2) {
   for (int i = 0; i < arr1.length; i++) {
     for (int j = 0; j < arr2.length; j++) {
@@ -30,6 +32,8 @@ bool containsCommonItem(List arr1, List arr2) {
   return false;
 }
 
+// O(a + b) Time Complexity
+// O(a) Space Complexity. Cuz We are creating a new map and adding arr1 into an map.
 bool containsCommonItem2(List arr1, List arr2) {
 // loop through first array and create object where properties === items in the array
   // can we assume always 2 params?
@@ -47,4 +51,10 @@ bool containsCommonItem2(List arr1, List arr2) {
     }
   }
   return false;
-}  // O(a + b) Time Complexity
+}
+
+// 3rd way is with built-in method
+
+bool containsCommonItem3(List arr1, List arr2) {
+  return arr1.any((item) => arr2.contains(item));
+}
