3ce279e4bc14f395beb8d0fcddc4dacbc956aaf7
diyorkhud <d1yorkhud@gmail.com>
2024-04-10 18:45:52 +0500

    better solution

--- example_interv_questions/examp2.dart
+++ example_interv_questions/examp2.dart
@@ -3,9 +3,12 @@ void main() {
   int sum = 8;
 
   print(hasPairWithSum(list, sum));
+  print(hasPairWithSum2(list, sum));
 }
 
 // brute force approach
+// Time Complexity O(n^2)
+// Space Complexity O(1)
 bool hasPairWithSum(List arr, num sum) {
   var len = arr.length;
   for (int i = 0; i < len - 1; i++) {
@@ -14,6 +17,24 @@ bool hasPairWithSum(List arr, num sum) {
         return true;
       }
     }
+  }
+  return false;
+}
+
+//better solution
+// Time Complexity O(n)
+// Space Complexity O(n)
+// Sets have constant-time average-case performance for operations
+// like adding and checking for element existence (add and contains methods),
+// making them suitable for this problem.
+bool hasPairWithSum2(List arr, num sum) {
+  Set mySet = {};
+  var len = arr.length;
+  for (int i = 0; i < len; i++) {
+    if (mySet.contains(arr[i])) {
+      return true;
+    }
+    mySet.add(sum - arr[i]);
   }
   return false;
 }
